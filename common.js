56373d8e9a8b568732390637528179312870c1a9
kukushka97 <66777964+kukushka97@users.noreply.github.com>
2021-12-05 16:57:55 +0300

    Add files via upload

--- common.js
+++ common.js
@@ -1,3 +1,3 @@
-let inputs = document.querySelectorAll('input[type="tel"]');
-let im = new Inputmask('+7 (999) 999-99-99');
-im.mask(inputs);
+$(document).ready(function() {
+	$("#phone").mask("+7 (999) 999-9999");
+});
